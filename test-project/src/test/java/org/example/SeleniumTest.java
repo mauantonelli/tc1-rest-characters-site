65dde0af7b29d22b807c18fa6e744b57243f230d
Gabriel <gabriel.rufino@biotronica.tech>
2024-06-06 11:40:55 -0300

    test: base url constant

--- test-project/src/test/java/org/example/SeleniumTest.java
+++ test-project/src/test/java/org/example/SeleniumTest.java
@@ -10,13 +10,11 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class SeleniumTest {
     private WebDriver driver;
+    private String BASE_URL = "https://tc1-rest-characters-site-m5ee.vercel.app/";
 
     @BeforeEach
     void setUp() {
         WebDriverManager.chromedriver().setup();
-        // There is a bug in ChromeDriver after Chrome updated to version 111.
-        // Add options to driver solve the problem, but it is a temporary workaround
-        // For more info: https://groups.google.com/g/chromedriver-users/c/xL5-13_qGaA
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
@@ -25,7 +23,7 @@ public class SeleniumTest {
     @Test
     void openProject() throws InterruptedException {
         final var softly = new SoftAssertions();
-        driver.get("https://tc1-rest-characters-site-m5ee.vercel.app/");
+        driver.get(BASE_URL);
         Thread.sleep(1000);
         String pageTitle = driver.getTitle();
         System.out.println("Page Title: " + pageTitle);
