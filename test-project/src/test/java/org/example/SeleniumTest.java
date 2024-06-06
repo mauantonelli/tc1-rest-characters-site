b79a17a248b93e9ffd2cc658e919a89389c5cf79
Gabriel <gabriel.rufino@biotronica.tech>
2024-06-06 12:59:41 -0300

    text: add setUpEdge for use in Gabriel's machine

--- test-project/src/test/java/org/example/SeleniumTest.java
+++ test-project/src/test/java/org/example/SeleniumTest.java
@@ -3,21 +3,34 @@ package org.example;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 
 public class SeleniumTest {
     private WebDriver driver;
     private String BASE_URL = "https://tc1-rest-characters-site-m5ee.vercel.app/";
 
+//    @BeforeEach
+//    void setUp() {
+//        WebDriverManager.chromedriver().setup();
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--remote-allow-origins=*");
+//        driver = new ChromeDriver(options);
+//    }
+
     @BeforeEach
-    void setUp() {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
+    void setUpEdge() {
+        WebDriverManager.edgedriver().setup();
+        EdgeOptions options = new EdgeOptions();
         options.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(options);
+        driver = new EdgeDriver(options);
     }
 
     @Test
