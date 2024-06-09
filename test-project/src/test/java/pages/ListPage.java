02298264f684833387b00222b9a35b7c0d3b0203
Gabriel <gabriel.rufino05@gmail.com>
2024-06-08 22:28:05 -0300

    feat: implementation of ListPage object

--- test-project/src/test/java/pages/ListPage.java
+++ test-project/src/test/java/pages/ListPage.java
@@ -1,4 +1,26 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class ListPage {
+    private WebDriver driver;
+    private By characterList = By.id("character-list");
+
+    public ListPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public String getCharacterInfo(int index) {
+        return driver.findElements(characterList).get(index).getText();
+    }
+
+    public void editCharacter(int index) {
+        driver.findElements(By.cssSelector(".edit")).get(index).click();
+    }
+
+    public void deleteCharacter(int index) {
+        driver.findElements(By.cssSelector(".delete")).get(index).click();
+        driver.switchTo().alert().accept();
+    }
 }
