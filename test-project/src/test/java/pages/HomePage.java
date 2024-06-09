a7fa94f8174641329795a7c0166d981aeec47372
Gabriel <gabriel.rufino05@gmail.com>
2024-06-08 22:26:14 -0300

    feat: implementation of HomePage object

--- test-project/src/test/java/pages/HomePage.java
+++ test-project/src/test/java/pages/HomePage.java
@@ -1,4 +1,37 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class HomePage {
+    private WebDriver driver;
+    private By nameField = By.id("name");
+    private By classField = By.id("class");
+    private By levelField = By.id("level");
+    private By submitButton = By.cssSelector("button[type='submit']");
+    private By viewListButton = By.xpath("//button[contains(text(),'Ver Lista de Personagens')]");
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void enterName(String name) {
+        driver.findElement(nameField).sendKeys(name);
+    }
+
+    public void enterClass(String className) {
+        driver.findElement(classField).sendKeys(className);
+    }
+
+    public void enterLevel(String level) {
+        driver.findElement(levelField).sendKeys(level);
+    }
+
+    public void submitForm() {
+        driver.findElement(submitButton).click();
+    }
+
+    public void viewCharacterList() {
+        driver.findElement(viewListButton).click();
+    }
 }
